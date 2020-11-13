228c85260f2cb7ed1ded1c8e036a0ca770c6ee75
Billon Tanguy <tanguy.billon@free.fr>
2020-11-13 18:15:07 +0100

    debut edtcontroller

--- GestionFormation/Controllers/EmploiDuTempsController.cs
+++ GestionFormation/Controllers/EmploiDuTempsController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GestionFormation.DTO;
+using GestionFormation.Entities;
+using GestionFormation.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,24 @@ namespace GestionFormation.Controllers
 {
     public class EmploiDuTempsController : Controller
     {
+        //L'emploi du temps de mon utilisateur
+        private EmploiDuTempsDTO EDT { get; set; }
+
+
+        public EmploiDuTempsController()
+        {
+            //On charge toutes les dates + formations + formateur dans mon controller
+            EDT = new EmploiDuTempsDTO(new UserDTO { Id = 1, Role = UserRole.ATTENDANT });
+        }
+
         // GET: EmploiDuTemps
         public ActionResult Index()
         {
+            DateTime now = DateTime.Now;
+
+            int WeekNumber = now.DayOfWeek;
+
+
             return View();
         }
     }
