40006a388622ac5a35026d7f3298f3451b0dd08e
Billon Tanguy <tanguy.billon@free.fr>
2020-11-18 10:16:16 +0100

    ca marche

--- GestionFormation/Controllers/EmploiDuTempsController.cs
+++ GestionFormation/Controllers/EmploiDuTempsController.cs
@@ -1,5 +1,6 @@
 ﻿using GestionFormation.DTO;
 using GestionFormation.Entities;
+using GestionFormation.Filters;
 using GestionFormation.Services;
 using System;
 using System.Collections.Generic;
@@ -10,26 +11,27 @@ using System.Web.Mvc;
 
 namespace GestionFormation.Controllers
 {
+    [LoginRequiredFilter]
     public class EmploiDuTempsController : Controller
     {
         //L'emploi du temps de mon utilisateur
         private EmploiDuTempsDTO EDT { get; set; }
-
 
         public EmploiDuTempsController()
         {
             //On charge toutes les dates + formations + formateur dans mon controller
 
             //Pour le moment, on charge l'emploi du temps de Hermione
-            EDT = new EmploiDuTempsDTO(new UserDTO { Id = 1, Role = UserRole.ATTENDANT});
-
-            //EDT = new EmploiDuTempsDTO((UserDTO)Session["userConnected"]);    => POURQUOI CA MARCHE PAS ??????
+            EDT = new EmploiDuTempsDTO((UserDTO)System.Web.HttpContext.Current.Session["userConnected"]);  
         }
+
 
         // GET: EmploiDuTemps
         public ActionResult Index()
         {
             List<JourneeDTO> WeeklyEDT = EmploiDuTempsService.GetWeek( EDT.ListDates, DateTime.Now);
+
+            ViewBag.User = (UserDTO)Session["userConnected"];   // => Pas de problème
 
             //Pour affichage et changement de semaine
             ViewBag.DateLundi = EmploiDuTempsService.DatePreviousMonday(DateTime.Now);
--- GestionFormation/Filters/LoginRequiredFilterAttribute.cs
+++ GestionFormation/Filters/LoginRequiredFilterAttribute.cs
@@ -32,5 +32,5 @@ namespace GestionFormation.Filters
                 );
             }
         }
-}
+    }
 }
