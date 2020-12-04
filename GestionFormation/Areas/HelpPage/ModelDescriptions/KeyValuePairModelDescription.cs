9916c5da5e6d253e9bf1e9b2b4786657bf5ca4a8
Vincent <billard.vincent@gmail.com>
2020-12-04 14:54:25 +0100

    Ajout CursusAPI et page help API

---
+++ GestionFormation/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace GestionFormation.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ GestionFormation/WebServices/CursusAPIController.cs
@@ -0,0 +1,45 @@
+﻿using GestionFormation.Entities;
+using GestionFormation.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GestionFormation.WebServices
+{
+    public class CursusAPIController : ApiController
+    {
+        // GET: api/CursusAPI
+        //[Route ("/cursus")]
+        public List<Cursus> Get()
+        {
+            return CursusService.FindAll();
+        }
+
+        // GET: api/CursusAPI/5
+        public Cursus Get(int id)
+        {
+            return CursusService.FindById(id);
+        }
+
+        // POST: api/CursusAPI
+        public void Post([FromBody] Cursus value)
+        {
+            CursusService.Create(value);
+
+        }
+
+        // PUT: api/CursusAPI/5
+        public void Put(int id, [FromBody] Cursus value)
+        {
+            if (id == value.CursusId) CursusService.Update(value);
+        }
+
+        // DELETE: api/CursusAPI/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
