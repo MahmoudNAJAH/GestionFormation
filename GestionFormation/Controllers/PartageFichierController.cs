d4a60098deb75a4eaf4989c91754d52680509159
Billon Tanguy <tanguy.billon@free.fr>
2020-12-14 14:38:01 +0100

    PartageFichier

--- GestionFormation/Controllers/PartageFichierController.cs
+++ GestionFormation/Controllers/PartageFichierController.cs
@@ -1,6 +1,7 @@
 ﻿using GestionFormation.DTO;
 using GestionFormation.Entities;
 using GestionFormation.Models;
+using GestionFormation.Services;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -33,7 +34,7 @@ namespace GestionFormation.Controllers
                 }); ;
             }
 
-            if (dirPath != null) model.DirectoryModel = GetDirectoryModel(dirPath);
+            if (dirPath != null) model.DirectoryModel = PartageFichierService.GetDirectoryModel(dirPath);
 
             //Dans le cas ou une seul SessionDeCursus, on pourrait rediriger dès maintenant vers le display du dossier
 
@@ -44,11 +45,11 @@ namespace GestionFormation.Controllers
         [HttpPost]
         public ActionResult Details(string dirPath)
         {
-            string combinedPath = GetFullPath(dirPath);
+            string combinedPath = PartageFichierService.GetFullPath(dirPath);
             //Pour les SessionDeCursus, si le dossier n'existe pas, on le créé
             if (!Directory.Exists(combinedPath)) Directory.CreateDirectory(combinedPath);
 
-            return PartialView("_Details", GetDirectoryModel(dirPath));
+            return PartialView("_Details", PartageFichierService.GetDirectoryModel(dirPath));
         }
 
         //On utiise une fonction différente pour faire les calculs ici et non dans le js
@@ -62,27 +63,28 @@ namespace GestionFormation.Controllers
                 int longeurDirPath = dirPath.Length;                //Le nombre de caractère dans mon DirPath
                                                                     //On veut le dirPath sans le dernier dossier
                                                                     //on prend du caractère 0 jusqu'au dernier dossier
-                dirPath = dirPath.Substring(0, longeurDirPath - longueur); //le -2 pour enlever les accolades
+                dirPath = dirPath.Substring(0, longeurDirPath - longueur); 
 
                 //Si on utilise le bouton à plusieurs reprise,
                 //Le dernier caractère est \\
                 //=> table[table.Length] == ""  => on ne change plus de folder
+                //Donc on enlève les accolades :
                 if (dirPath[dirPath.Length - 1] == '\\') dirPath = dirPath.Substring(0, dirPath.Length - 1);
 
             }
 
-            string combinedPath = GetFullPath(dirPath); 
+            string combinedPath = PartageFichierService.GetFullPath(dirPath); 
             //Pour les SessionDeCursus, si le dossier n'existe pas, on le créé
             if (!Directory.Exists(combinedPath)) Directory.CreateDirectory(combinedPath);
 
-            return PartialView("_Details", GetDirectoryModel(dirPath));
+            return PartialView("_Details", PartageFichierService.GetDirectoryModel(dirPath));
         }
 
         // GET: PartageFichier/Create
         public ActionResult CreateDirectory(string dirPath, string dirName)
         {
             //On créé le dossier ave
-            Directory.CreateDirectory(Path.Combine(GetFullPath(dirPath), dirName));
+            Directory.CreateDirectory(Path.Combine(PartageFichierService.GetFullPath(dirPath), dirName));
 
             //Même si on a créé le dossier, on reste dans le dossier parent pour l'affichage
             return RedirectToAction("Index", "PartageFichier", new { dirPath = dirPath });
@@ -93,7 +95,7 @@ namespace GestionFormation.Controllers
         {
             if(myFile != null)
             {
-                string combinedPath = Path.Combine(GetFullPath(dirPath), Path.GetFileName(myFile.FileName));
+                string combinedPath = Path.Combine(PartageFichierService.GetFullPath(dirPath), Path.GetFileName(myFile.FileName));
 
                 // sauvegarde sur le serveur
                 myFile.SaveAs(combinedPath);
@@ -102,35 +104,24 @@ namespace GestionFormation.Controllers
             return RedirectToAction("Index", "PartageFichier", new { dirPath = dirPath });
         }
 
+        public ActionResult DeleteDirectory(string dirPath, string dirName)
+        {
+            PartageFichierService.DeleteFolder(Path.Combine(PartageFichierService.GetFullPath(dirPath), dirName));
+
+            return RedirectToAction("Index", "PartageFichier", new { dirPath = dirPath });
+        }
+
+        public ActionResult DeleteFFile(string dirPath, string fileName)
+        {
+            System.IO.File.Delete(Path.Combine(PartageFichierService.GetFullPath(dirPath), fileName));
+
+            return RedirectToAction("Index", "PartageFichier", new { dirPath = dirPath });
+        }
+
         public FileResult Download(string fileName)
         {
-            byte[] fileBytes = System.IO.File.ReadAllBytes(GetFullPath(fileName));
+            byte[] fileBytes = System.IO.File.ReadAllBytes(PartageFichierService.GetFullPath(fileName));
             return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
         }
-
-        private string GetFullPath(string dir)
-        {
-            //Pour permettre l'écriture de fichier en toute sécurité
-            //Le chemin vers le dossier ~/Saves est décrit dans le Web.config
-            string relativePath = Path.Combine(ConfigurationManager.AppSettings["filePath"], dir);
-            //Pour éviter certains soucis de chemin
-            string combinedPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
-
-            return combinedPath;
-        }
-
-        private DirectoryModel GetDirectoryModel(string dirPath)
-        {
-            DirectoryModel result = new DirectoryModel();
-
-            string combinedPath = GetFullPath(dirPath);
-
-            result.DirPath = dirPath;
-            foreach (string str in Directory.EnumerateDirectories(combinedPath)) result.Directories.Add(str.Split('\\')[str.Split('\\').Length - 1]);
-            foreach (string str in Directory.EnumerateFiles(combinedPath)) result.Files.Add(str.Split('\\')[str.Split('\\').Length - 1]);
-
-            return result;
-        }
-
     }
 }
