e74794926b942ca0655b80e8dc020493529d4a90
Yosra Houimli <houimliyosra.b@gmail.com>
2020-12-15 14:23:40 +0100

    feuille d'éval

--- GestionFormation/DTO/FicheEval.cs
+++ GestionFormation/DTO/FicheEval.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,25 +8,26 @@ namespace GestionFormation.DTO
 {
     public class FicheEval
     {
-  
 
 
-       public  string fiche1 { get; set; }
-       public  string fiche2 { get; set; }
-       public  string fiche3 { get; set; }
-       public  string fiche4 { get; set; }
-       public  string fiche5 { get; set; }
-       public  string fiche6 { get; set; }
-       public  string fiche7 { get; set; }
-       public  string fiche8 { get; set; }
-       public  string fiche9 { get; set; }
-       public  string fiche10 { get; set; }
-       public  string fiche11{ get; set; }
-       public  string fiche12{ get; set; }
-       public  string fiche13{ get; set; }
-       public  string fiche14{ get; set; }
-       public  string fiche15{ get; set; }
-       public  string fiche16{ get; set; }
+        
+        public string fiche1 { get; set; }
+        public string fiche2 { get; set; }
+        public string fiche3 { get; set; }
+        public string fiche4 { get; set; }
+       
+        public string fiche5 { get; set; }
+        public string fiche6 { get; set; }
+        public string fiche7 { get; set; }
+        public string fiche8 { get; set; }
+        public string fiche9 { get; set; }
+        public string fiche10 { get; set; }
+        public string fiche11 { get; set; }
+        public string fiche12 { get; set; }
+        public string fiche13 { get; set; }
+        public string fiche14 { get; set; }
+        public string fiche15 { get; set; }
+        public string fiche16 { get; set; }
         public string vb { get; set; }
 
 
@@ -48,11 +50,11 @@ namespace GestionFormation.DTO
 
 
 
-        public bool Excellent{ get; set; }
-        public bool TresBon{ get; set; }
-        public bool Bon{ get; set; }
-        public bool Moyen {get; set; }
-        public bool Passable  {get; set; }
+        public bool Excellent { get; set; }
+        public bool TresBon { get; set; }
+        public bool Bon { get; set; }
+        public bool Moyen { get; set; }
+        public bool Passable { get; set; }
 
         public bool Excellent1 { get; set; }
         public bool TresBon1 { get; set; }
@@ -62,14 +64,14 @@ namespace GestionFormation.DTO
 
         public bool Excellent2 { get; set; }
         public bool TresBon2 { get; set; }
-        public bool Bon2{ get; set; }
+        public bool Bon2 { get; set; }
         public bool Moyen2 { get; set; }
         public bool Passable2 { get; set; }
 
 
         public bool Excellent3 { get; set; }
-        public bool TresBon3{ get; set; }
-        public bool Bon3{ get; set; }
+        public bool TresBon3 { get; set; }
+        public bool Bon3 { get; set; }
         public bool Moyen3 { get; set; }
         public bool Passable3 { get; set; }
 
@@ -82,48 +84,48 @@ namespace GestionFormation.DTO
 
         public bool Excellent5 { get; set; }
         public bool TresBon5 { get; set; }
-        public bool Bon5{ get; set; }
+        public bool Bon5 { get; set; }
         public bool Moyen5 { get; set; }
-        public bool Passable5{ get; set; }
+        public bool Passable5 { get; set; }
 
 
         public bool Excellent6 { get; set; }
         public bool TresBon6 { get; set; }
-        public bool Bon6{ get; set; }
-        public bool Moyen6{ get; set; }
+        public bool Bon6 { get; set; }
+        public bool Moyen6 { get; set; }
         public bool Passable6 { get; set; }
 
         public bool Excellent7 { get; set; }
         public bool TresBon7 { get; set; }
-        public bool Bon7{ get; set; }
-        public bool Moyen7{ get; set; }
-        public bool Passable7{ get; set; }
+        public bool Bon7 { get; set; }
+        public bool Moyen7 { get; set; }
+        public bool Passable7 { get; set; }
 
 
 
-        public bool Excellent8{ get; set; }
+        public bool Excellent8 { get; set; }
         public bool TresBon8 { get; set; }
         public bool Bon8 { get; set; }
         public bool Moyen8 { get; set; }
-        public bool Passable8{ get; set; }
+        public bool Passable8 { get; set; }
 
 
 
-        public bool Excellent9{ get; set; }
-        public bool TresBon9{ get; set; }
+        public bool Excellent9 { get; set; }
+        public bool TresBon9 { get; set; }
         public bool Bon9 { get; set; }
-        public bool Moyen9{ get; set; }
-        public bool Passable9{ get; set; }
+        public bool Moyen9 { get; set; }
+        public bool Passable9 { get; set; }
 
 
         public bool Excellent10 { get; set; }
-        public bool TresBon10{ get; set; }
-        public bool Bon10{ get; set; }
-        public bool Moyen10{ get; set; }
-        public bool Passable10{ get; set; }
+        public bool TresBon10 { get; set; }
+        public bool Bon10 { get; set; }
+        public bool Moyen10 { get; set; }
+        public bool Passable10 { get; set; }
 
 
-        public bool Excellent11{ get; set; }
+        public bool Excellent11 { get; set; }
         public bool TresBon11 { get; set; }
         public bool Bon11 { get; set; }
         public bool Moyen11 { get; set; }
@@ -132,12 +134,12 @@ namespace GestionFormation.DTO
         public bool Excellent12 { get; set; }
         public bool TresBon12 { get; set; }
         public bool Bon12 { get; set; }
-        public bool Moyen12{ get; set; }
-        public bool Passable12{ get; set; }
-        public string Question{ get; set; }
-        public string Question1{ get; set; }
-        public string Question2{ get; set; }
-        public string  Question3{ get; set; }
+        public bool Moyen12 { get; set; }
+        public bool Passable12 { get; set; }
+        public string Question { get; set; }
+        public string Question1 { get; set; }
+        public string Question2 { get; set; }
+        public string Question3 { get; set; }
 
 
         public string Commentaire { get; set; }
@@ -146,6 +148,10 @@ namespace GestionFormation.DTO
         public string Commentaire3 { get; set; }
         public string Commentaire4 { get; set; }
 
+        public bool checked1 { get; set; }
+        public bool checked2 { get; set; }
+        public bool checked3 { get; set; }
+        public bool checked4 { get; set; }
     }
 
 }
